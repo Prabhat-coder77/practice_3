426b595456fd386a06b00eac87b860de2210edc6
prabhat.p.prabhatgouda281@gmail.com <prabhatgouda281@gmail.com>
2025-10-02 16:17:09 +0530

    First commit

---
+++ src/main/java/com/test_git/TestGitApplication.java
@@ -0,0 +1,16 @@
+package com.test_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+
+	public static void main(String[] args) {
+        String s="prabhat";
+        System.out.println(s);
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+
+}
